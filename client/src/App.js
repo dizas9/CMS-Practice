b5ebe5417229de287a4d8f1df38373c9005e1c87
dizas9 <sazid.ice07@gmail.com>
2023-10-22 08:17:05 +0600

    Hello Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import Layout from "./pages/Layout/layout";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-<>
-<Layout/>
-</>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />} />
+          <Route index element={<Layout />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
